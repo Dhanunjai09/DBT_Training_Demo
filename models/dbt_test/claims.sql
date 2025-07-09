b088f72559582d043725dc4d12db632d44c4f43c
Dhanunjai09 <dhanunjai09@gmail.com>
2025-07-09 12:48:15 +0530

    updated the cliams model with policy id

--- models/dbt_test/claims.sql
+++ models/dbt_test/claims.sql
@@ -1,2 +1,7 @@
 
+<<<<<<< HEAD
 SELECT * FROM {{ ref('claims_seed') }}  order by claim_id
+=======
+SELECT * FROM {{ ref('claims_seed') }}
+order by Policy_Id
+>>>>>>> 7ae2dfe (updated the cliams model with policy id)
